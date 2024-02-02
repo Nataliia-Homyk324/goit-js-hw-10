70b130e45faea74ed73b853550512149372fa06b
Nataliia-Homyk324 <homyk324@gmail.com>
2024-02-02 23:11:01 +0200

    timer-5

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,15 +1,19 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import icon from "../img/octagon.png";
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-let userSelectedDate;// Declaring the variable outside of onClose() method
+
+
+let userSelectedDate = null;// Declaring the variable outside of onClose() method
 let startButton = document.querySelector(".start"); 
 
 startButton.disabled = true;
 startButton.classList.add('inactive');
 startButton.classList.remove('active');
+
 
 
 const options = {
@@ -18,20 +22,22 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-     userSelectedDate = selectedDates[0];
-     console.log(userSelectedDate);
-    if (userSelectedDate < new Date()) {
+    userSelectedDate = selectedDates[0];
+    console.log(userSelectedDate);
+    
+     if (userSelectedDate < new Date()) {
       iziToast.error({
         message: "Please choose a date in the future",
         width: 300, height: 64, position: "topRight",
         timeout: false, closeOnEscape: true,
         messageSize: 16,
-        messageColor: '	#000000',
+        messageColor: '	#fff',
         backgroundColor: '#ef4040',
         title: 'Error',
         titleSize: 16,
-        titleColor: '	#000000',
-        iconColor: "#fff",
+        titleColor: '	#fff',
+        iconUrl: icon,
+        iconColor:"#fff",
       })
       startButton.disabled = true;
       startButton.classList.remove('active');
@@ -45,6 +51,28 @@ const options = {
 };
 
 flatpickr("#datetime-picker", options); 
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
 
 
 

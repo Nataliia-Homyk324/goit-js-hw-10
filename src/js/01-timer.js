43d2d921e98aeec901ff24a480a4c3e3f5379bb1
Nataliia-Homyk324 <homyk324@gmail.com>
2024-02-03 15:59:46 +0200

    timer-finish

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,12 +1,13 @@
+//libraries
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import icon from "../img/octagon.png";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+// icon for message
+import icon from "../img/octagon.png";
 
-
-
-let userSelectedDate;
+let userSelectedDate;// оголосили глобально, бо це зачення буде потрібне поза межами методу onClose() об'єкта options 
+// надаєм доступ в коді до  DOM елементів  
 let startButton = document.querySelector("[data-start]"); 
 let timerFieldDays = document.querySelector("[data-days]");
 let timerFieldHours = document.querySelector("[data-hours]");
@@ -14,20 +15,25 @@ let timerFieldMinutes = document.querySelector("[data-minutes]");
 let timerFieldSeconds = document.querySelector("[data-seconds]");
 let input = document.querySelector(".inp");
 
-
+// визначаєм початковий стан кнопки та поля введення після завантаження сторінки
 startButton.disabled = true;
 startButton.classList.add('inactive');
 startButton.classList.remove('active');
 input.disabled = false;
 
-let timerId = null;
+let timerId = null;// Змінна timerId  це цифровий ідентифікатор  таймера створеного методом setInterval(), яка потім буде використаним для його подальшого скасування 
 
-
+// опції елемента ініціалізованого бібліотекою flatpickr, який надає можливість обрати час на сторінці в рядку повідомлення
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  // при закриті елемента, ініціалізованого на інпуті,
+  // ми передбачаєм у випадку обрання дати в минулому часі 
+  // ініціалізацію елемента бібліотеки izitoast й задаєм 
+  // йому опції та відповідно до вибору користувача налаштовуємо
+  // стан кнопки та рядка введення дати
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0];
     //  console.log(userSelectedDate);
@@ -57,7 +63,7 @@ const options = {
   },
 };
 
- 
+ // функція  повертає обраний час  у вигляді об'єкта
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -74,36 +80,41 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-// time formatting
+// форматуєм значення властивостей { days, hours, minutes, seconds } 01, 02 замість 1, 2 і т.д.
 function addLeadingZero(value) {
   return String(value).padStart(2, 0);
 }
-
+// обновлюєм покази таймера з інтервалом 1сек й зупиняєм його, якщо залишок часу 0
 function onTimerStart() {
-  const selectedDate = flatp.selectedDates[0];
-  timerId = setInterval(() => {
-    const startTime = new Date();
-    const countdown = selectedDate - startTime;
-    startButton.disabled = true;
-    input.disabled = true;
+  // запускаєм повторювальну дію з інтервалом 1 сек
+    timerId = setInterval(() => {
+    const startTime = new Date();// визначаєм поточну дату
+    const countdown = userSelectedDate - startTime;//залишок часу
+    // змінюємо стан кнопки та рядка введення 
+    startButton.disabled = true;// кнопка старт стає неактивна
+    input.disabled = true;// рядок  введення стає неактивним
     if (countdown < 0) {
-      clearInterval(timerId);
-      input.disabled = false;
-      startButton.disabled = false;
-      return;
+      clearInterval(timerId);// припинення виконання повторюваних дій, які були запущені за допомогою функції setInterval()
+      input.disabled = false;//рядок введення стає активним для вибору нового часу
+      startButton.disabled = false;// кнопка стає активною
+      return;// залишок часу стає 0, функція припиняє роботу
     }
-    updateTimerFace(convertMs(countdown));
-  }, 1_000);
+    updateTimerFace(convertMs(countdown));// викликаєм функцію для зміни показів таймера на сторінці
+  }, 1000);// задаєм інтервал оновлення показів на сторінці 1 секунда в милісекундах, другий аргумент методу, першим є колбек функція
 }
-
+// виведення значень властивостей { days, hours, minutes, seconds } на сторінку
 function updateTimerFace({ days, hours, minutes, seconds }) {
   timerFieldDays.textContent = addLeadingZero(days);
   timerFieldHours.textContent = addLeadingZero(hours);
   timerFieldMinutes.textContent = addLeadingZero(minutes);
   timerFieldSeconds.textContent = addLeadingZero(seconds);
 }
-
+// ініціалізація бібліотеки
+// на елементі в кінці скрипта, спочатку я зробила це після об'єкта
+// options й значення змінної userSelectedDate в наступному коді стало 
+// невизначеним
 const flatp = flatpickr("#datetime-picker", options); 
+// додаєм до кнопки слухача події клік
 startButton.addEventListener("click", onTimerStart);
 
 
